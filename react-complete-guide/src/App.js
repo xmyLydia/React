2877eb48539a10d2da8c62385054650c62788b69
mxie <836728902@qq.com>
2019-12-07 00:06:42 +0200

    react features

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, world</h1>
+        <h2>test</h2>
+        <Person />
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi'));
+  }
+}
+
+export default App;
