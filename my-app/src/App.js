9c5fe4dd0de1c9a812112281dffee1d96fed2465
mxie <836728902@qq.com>
2020-01-05 14:36:14 +0200

    starting my app

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
+function App() {
+  return (
+    <div >
+      <Layout>
+        <BurgerBuilder/>
+
+        
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
