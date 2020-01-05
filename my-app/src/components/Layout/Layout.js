aef1a6f00a07133ebc1e452754800b35d005010f
mxie <836728902@qq.com>
2020-01-05 17:48:10 +0200

    adding burger ingredient dynamically

--- my-app/src/components/Layout/Layout.js
+++ my-app/src/components/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
-import classes from '../Layout/Layout.css'
+import classes from '../Layout/Layout.module.css'
 
 const layout = (props) => (
     <Aux>
--- my-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ my-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 import Aux from "../../hoc/Auxiliary";
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component{
+    // constructor(props){
+    //     super(props);
+    //     this.state = {}
+    // }
+    state = {
+        ingredients:{
+            salad: 1,
+            bacon: 1,
+            cheese:2,
+            meat:0
+        }
+    }
     render(){
         return (
            <Aux>
-               <div>Burger</div>
+               <Burger ingredients={this.state.ingredients}></Burger>
                <div>Builder Controls</div>
            </Aux>
         );
