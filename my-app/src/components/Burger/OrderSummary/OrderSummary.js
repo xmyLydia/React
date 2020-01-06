a0c6de49c89e83db66e601547703e0a4d5a542d3
mxie <836728902@qq.com>
2020-01-06 17:30:58 +0200

    optimize and re-structure

--- my-app/src/components/Burger/OrderSummary/OrderSummary.js
+++ my-app/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,35 +1,36 @@
-import React from 'react';
-import Aux from '../../../hoc/Auxiliary';
+import React, { Component } from 'react';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(idKey => {
-            return <li key={idKey}>
-                <span style={{ textTransform: 'capitalize' }}>
-                    {idKey}:
+class OrderSummary extends Component { 
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(idKey => {
+                return <li key={idKey}>
+                    <span style={{ textTransform: 'capitalize' }}>
+                        {idKey}:
                     </span>
-                {props.ingredients[idKey]}
-            </li>
-        })
-    return (
-        <Aux>
+                    {this.props.ingredients[idKey]}
+                </li>
+            })
+        return (<Aux>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+            <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
             <p>Continue to check out?</p>
             <Button
                 btnType="Danger"
-                clicked={props.purchaseCanceled}
+                clicked={this.props.purchaseCanceled}
             >CANCEL</Button>
             <Button
                 btnType="Success"
-                clicked={props.purchaseContinued}
+                clicked={this.props.purchaseContinued}
             >CONTINUE</Button>
-        </Aux>
-    )
+        </Aux>);
+    } 
 };
-export default orderSummary;
+export default OrderSummary;
