4d287ca0d7ce77674d189524dedff83778beb87f
mxie <836728902@qq.com>
2020-01-06 14:04:42 +0200

    css styling for mobile

--- my-app/src/components/Layout/Layout.js
+++ my-app/src/components/Layout/Layout.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from '../Layout/Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component  {
+    state={
+        showSideDrawer: true
+    }
+    sideDrawerClosedHandler=()=>{
+        this.setState({showSideDrawer: false});
+    }
+    render(){
+        return (<Aux>
+            <Toolbar />
+            <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+        </Aux>
+        );
+    }
+    
+};
 
-export default layout;
+export default Layout;
