297bb7c581d913a7e7f22079bdaaf197c14aee55
mxie <836728902@qq.com>
2020-01-08 20:30:50 +0200

    get data through http and show it in UI

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -7,22 +7,43 @@ import './Blog.css';
 import axios from 'axios';
 
 class Blog extends Component {
+    state = {
+        posts: [],
+        selectedPostId: null
+    }
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                console.log(response);
+                const posts_data = response.data.slice(0, 4);
+                const updatePosts = posts_data.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                });
+                this.setState({ posts: updatePosts })
             });
     }
+    postSelectedHandler = (id) => {
+        this.setState({ selectedPostId: id });
+    }
     render() {
+        const posts = this.state.posts.map(post => {
+            return <Post
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)}
+            />
+        }
+        );
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
