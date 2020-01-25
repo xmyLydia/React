1325eec22f18f70078460411dafec33711d56b48
mxie <836728902@qq.com>
2020-01-26 00:27:52 +0200

    advanced redux

--- redux--01-start/src/store/reducers/counter.js
+++ redux--01-start/src/store/reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../action/actionTypes';
+import { updateObject } from '../reducers/utility';
 
 const initialState = {
     counter: 0
@@ -6,25 +7,19 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.INCREMENT:
-            const newState = Object.assign({}, state);
-            newState.counter = state.counter + 1;
-            return newState;
+            // const newState = Object.assign({}, state);
+            // newState.counter = state.counter + 1;
+            // return newState;
+            return updateObject(state, { counter: state.counter + 1 })
 
         case actionTypes.DECREMENT:
-            return {
-                ...state,
-                counter: state.counter - 1
-            }
+            return updateObject(state, { counter: state.counter - 1 });
+
         case actionTypes.ADD:
-            return {
-                ...state,
-                counter: state.counter + action.value
-            }
+            return updateObject(state, { counter: state.counter + action.value });
+
         case actionTypes.SUBTRACT:
-            return {
-                ...state,
-                counter: state.counter - action.value
-            }
+            return updateObject(state, { counter: state.counter - action.value })
     }
     return state;
 }
