6b323d3c76dba6476c9446b4b9e259e406563973
mxie <836728902@qq.com>
2020-01-28 21:05:19 +0200

    combine reducers in burger project

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,13 +5,19 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
+import orderReducer from './store/reducers/order';
 import thunk from 'redux-thunk';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+    burgerBuilder: burgerBuilderReducer,
+    order: orderReducer
+})
 const store = createStore(
-    burgerBuilderReducer,
+    rootReducer,
     composeEnhancers(
         applyMiddleware(thunk)
     )
--- my-app/src/store/actions/order.js
+++ my-app/src/store/actions/order.js
@@ -20,7 +20,7 @@ export const purchaseBurger = (orderData) => {
         dispatch(purchaseBurgerStart());
         axios.post('/orders.json', orderData)
             .then(response => {
-                dispatch(purchaseBurgerSuccess(response.data, orderData))
+                dispatch(purchaseBurgerSuccess(response.data.name, orderData))
 
             })
             .catch(error => {
