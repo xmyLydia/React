93dd67727a1762ca67d9bc87ef01dacfc00340b9
mxie <836728902@qq.com>
2020-02-04 22:23:56 +0200

    fix issues overview

--- my-app/src/components/Navigation/SideDrawer/SideDrawer.js
+++ my-app/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -7,18 +7,20 @@ import Aux from '../../../hoc/Auxiliary/Auxiliary';
 
 const sideDrawer = (props) => {
     let attachedClasses = [classes.SideDrawer, classes.Close];
-    if(props.open){
+    if (props.open) {
         attachedClasses = [classes.SideDrawer, classes.Open];
     }
     return (
         <Aux>
             <BackDrop show={props.open} clicked={props.closed} />
-            <div className={attachedClasses.join(' ')}>
+            <div className={attachedClasses.join(' ')}
+                onClick={props.closed}
+            >
                 <div className={classes.Logo}>
                     <Logo />
                 </div>
                 <nav>
-                    <NavigationItems isAuthenticated = {props.isAuth}/>
+                    <NavigationItems isAuthenticated={props.isAuth} />
                 </nav>
             </div>
         </Aux>
--- my-app/src/store/reducers/order.js
+++ my-app/src/store/reducers/order.js
@@ -13,7 +13,7 @@ const purchaseInit = (state, action) => {
 };
 const purchaseBurgerStart = (state, action) => {
     return updateObject(state, {
-        purchased: true
+        loading: true
     });
 };
 const purchaseBurgerSuccess = (state, action) => {
