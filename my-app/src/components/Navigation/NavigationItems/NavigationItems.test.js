f287ad1813ba91051be4becf0dfb62b204a8c98b
mxie <836728902@qq.com>
2020-02-09 01:02:00 +0200

    test container and redux

--- my-app/src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ my-app/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -7,9 +7,26 @@ import React from 'react';
 configure({ adapter: new Adapter() });
 
 describe('<NavigationItems />', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />);
+    })
+
     it('should render two <NavigationItems /> elements if not authenticated',
         () => {
-            const wrapper = shallow(<NavigationItems />);
             expect(wrapper.find(NavigationItem)).toHaveLength(2);
+        });
+    it('should render three <NavigationItems /> elements if authenticated',
+        () => {
+            //wrapper = shallow(<NavigationItems isAuthenticated/>);
+            wrapper.setProps({ isAuthenticated: true });
+            expect(wrapper.find(NavigationItem)).toHaveLength(3);
+        });
+
+    it('should render logout <NavigationItems /> elements if authenticated',
+        () => {
+            wrapper.setProps({ isAuthenticated: true });
+            expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>))
+            .toEqual(true);
         });
 });
