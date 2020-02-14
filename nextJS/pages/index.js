3b262dc0127feac882f1c44112b3c1756475a760
mxie <836728902@qq.com>
2020-02-14 21:17:46 +0200

    next js router

--- nextJS/pages/index.js
+++ nextJS/pages/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
-const indexPage = ()=>(
+import Link from 'next/link';
+import Router from 'next/router';
+
+const indexPage = () => (
     <div>
         <h1>The Main Page</h1>
+        <p>Go to <Link href="/auth"><a>Auth</a></Link></p>
+        <button onClick={()=>Router.push('/auth')}>Go to Auth</button>
     </div>
 );
 
