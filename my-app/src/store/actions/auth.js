bac63d45228196029cdb81e12c5323213cdc6a88
mxie <836728902@qq.com>
2020-02-23 21:25:41 +0200

    saga in more actions

--- my-app/src/store/actions/auth.js
+++ my-app/src/store/actions/auth.js
@@ -28,11 +28,15 @@ export const logout = () => {
         type: actionTypes.AUTH_INITIATE_LOGOUT
     }
 }
+export const logoutSucceed = () => {
+    return {
+        type: actionTypes.AUTH_INITIATE_LOGOUT
+    }
+}
 export const checkAuthTimeout = (expirationTime) => {
-    return dispatch => {
-        setTimeout(() => {
-            dispatch(logout());
-        }, expirationTime * 1000);
+    return {
+        type: actionTypes.AUTH_CHECK_TIMEOUT,
+        expirationTime: expirationTime
     }
 }
 export const auth = (email, password, isSignup) => {
@@ -49,7 +53,7 @@ export const auth = (email, password, isSignup) => {
         }
         axios.post(url, authData)
             .then(response => {
-              
+
                 const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
                 localStorage.setItem('token', response.data.idToken);
                 localStorage.setItem('expirationDate', expirationDate);
@@ -78,7 +82,7 @@ export const authCheckState = () => {
         } else {
             const expirationDate = new Date(localStorage.getItem('expirationDate'));
             if (expirationDate <= new Date()) {
-               dispatch(logout());
+                dispatch(logout());
             } else {
                 const userId = localStorage.getItem('userId');
                 const expriationTime = (expirationDate.getTime() - (new Date().getTime())) / 1000;
--- my-app/src/store/sagas/index.js
+++ my-app/src/store/sagas/index.js
@@ -1,7 +1,8 @@
-import {logoutSaga} from './auth';
+import {logoutSaga, checkAuthTimeoutSaga} from './auth';
 import * as actionTypes from '../actions/actionTypes';
 import {takeEvery} from 'redux-saga/effects';
 
 export function* watchAuth(){
     yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
+    yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
 }
