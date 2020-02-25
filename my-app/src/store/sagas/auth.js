216d16d430a93ddbb631dcfe402a94e47c0ee5f1
mxie <836728902@qq.com>
2020-02-25 21:17:55 +0200

    try to find out problem with saga

--- my-app/src/store/sagas/auth.js
+++ my-app/src/store/sagas/auth.js
@@ -1,15 +1,12 @@
 import { put } from 'redux-saga/effects';
 import * as actions from '../actions/index';
-import * as actionTypes from '../actions/actionTypes';
 import delay from 'redux-saga';
 
 export function* logoutSaga(action) {
     yield localStorage.removeItem('token');
     yield localStorage.removeItem('expirationDate');
     yield localStorage.removeItem('userId');
-    yield put({
-        type: actionTypes.AUTH_LOGOUT
-    });
+    yield put(actions.logoutSucceed());
 };
 export function* checkAuthTimeoutSaga(action){
    yield delay(action.expirationTime * 1000);
