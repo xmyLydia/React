9b79a49179094e97565654802ca4dc0f66875244
mxie <836728902@qq.com>
2020-03-01 01:12:48 +0200

    remove element from list in useState

--- hooks-01-starting-project/src/components/Ingredients/IngredientForm.js
+++ hooks-01-starting-project/src/components/Ingredients/IngredientForm.js
@@ -4,10 +4,12 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  const inputState = useState({ title: '', amount: '' });
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+
   const submitHandler = event => {
     event.preventDefault();
-    // ...
+    props.onAddIngredient({title: enteredTitle, amount: enteredAmount});
   };
 
   return (
@@ -16,26 +18,18 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={inputState[0].title}
-              onChange={event =>{
-                const newTitle = event.target.value;
-                inputState[1](prevInputState =>
-                  ({
-                    title: newTitle,
-                    amount: prevInputState.amount
-                  }))}}
-                   />
+            <input type="text" id="title" value={enteredTitle}
+              onChange={event => {
+               setEnteredTitle(event.target.value);
+              }}
+            />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={inputState[0].amount}
+            <input type="number" id="amount" value={enteredAmount}
               onChange={event => {
-                const newAmount = event.target.value;
-                inputState[1](prevInputState =>
-                  ({
-                    amount: newAmount,
-                    title: prevInputState.title
-                  }))}} />
+               setEnteredAmount(event.target.value);
+              }} />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
