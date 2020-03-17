eb4bb827d7e312c3dc7825bb4acaac316b784063
mxie <836728902@qq.com>
2020-03-17 21:35:31 +0200

    custome hooks

---
+++ replace-redux-01-starting-project/src/hooks-store/store.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+let globalState = {};
+let listeners = [];
+let actions = {};
+
+export const useStore = () => {
+    const setState = useState(globalState)[1];
+
+    const dispatch = actionIdentifier => {
+        const newState = actions[actionIdentifier](globalState);
+        globalState = { ...globalState, ...newState };
+
+        for (const listener of listeners) {
+            listener(globalState);
+        }
+    }
+    useEffect(() => {
+        listeners.push(setState);
+        return () => {
+            listeners = listeners.filter(li => li !== setState);
+        }
+    }, [setState]);
+    return [globalState, dispatch];
+};
+export const initStore = (userAction, initialState) => {
+    if (initialState) {
+        globalState = { ...globalState, ...initialState };
+    }
+    actions = { ...actions, ...userAction };
+};
